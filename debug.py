95a62fa48b5f66d0b712e4d0c7d3358c5c9ed18c
Afnan Shomar <afnanshomar@192.168.1.5>
2025-02-14 22:43:47 +0200

    Updated debug.py after testing

--- debug.py
+++ debug.py
@@ -7,12 +7,12 @@ The function should compute each of the metrics for the prediction and sum them.
 It should also add to this sum three default metrics, in this case, adding 0, 1 or 2 to the prediction.
 """
 
-
 def get_sum_metrics(prediction, metrics=None):
     if not metrics:
         metrics = []
+
     for i in range(3):
-        metrics.append(gen_add_i(i))
+        metrics.append(gen_add_i(i))  # Append lambda functions to metrics
 
     sum_metrics = 0
     for metric in metrics:
@@ -22,3 +22,12 @@ def get_sum_metrics(prediction, metrics=None):
 
 def gen_add_i(i):
     return lambda x: x + i
+print(get_sum_metrics(0))
+print(get_sum_metrics(1))
+print(get_sum_metrics(2))
+print(get_sum_metrics(3))
+print(get_sum_metrics(0))
+print(get_sum_metrics(1))
+print(get_sum_metrics(2))
+
+
